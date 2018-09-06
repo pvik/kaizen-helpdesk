71a3210a2441bd8e3e19b308c15fd4457d1b27bf
Praveen Vikram <praveen.vikram@kiosa.org>
2018-09-05 23:06:38 -0400

    adding permission migrations

---
+++ migrations/060-create-permission-rules.down.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS kaizen.permission_rule CASCADE;
+--;;
+DROP TABLE IF EXISTS kaizen.permission_group CASCADE;
+--;;
+DROP TABLE IF EXISTS kaizen.permission_group_member CASCADE;
+--;;
+DROP TABLE IF EXISTS kaizen.permission_assignment CASCADE;
+--;;
+DROP TABLE IF EXISTS kaizen.permission_group_assignment CASCADE;
+--;;
